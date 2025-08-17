3e19756615d257ae110c64ce2b92910c20197535
TheHaccon <matcote111@gmail.com>
2025-08-17 09:42:28 -0400

    Update players-app.js
    Aoba Johsai done

--- players-app.js
+++ players-app.js
@@ -531,7 +531,6 @@
       },
       tags: ["Wing Spiker", "Karasuno", "R"]
     },
-    ,
     {
       id: "kazuhito",
       name: "Kazuhito Narita",
@@ -559,6 +558,510 @@
         }
       },
       tags: ["Middle Blocker", "Karasuno", "R"]
+    },
+    {
+      id: "toru(Practice)",
+      name: "Toru Oikawa (Practice)",
+      img: "img-char/oikawaSSR.png",
+      school: "Aoba Johsai",
+      rarity: "SSR",
+      position:"Setter",
+      stats: {
+        lvl1: {
+          Serve: 122,
+          Power_Attack: 110,
+          Set: 116,
+          Receive: 98,
+          Block: 109,
+          Save: 92
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Setter", "Aoba Johsai", "SSR"]
+    },
+    {
+      id: "iwaizumi",
+      name: "Hajime Iwaizumi",
+      img: "img-char/iwaizumiSSR.png",
+      school: "Aoba Johsai",
+      rarity: "SSR",
+      position:"Wing Spiker",
+      stats: {
+        lvl1: {
+          Serve: 104,
+          Power_Attack: 121,
+          Set: 98,
+          Receive: 115,
+          Block: 109,
+          Save: 99
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Wing Spiker", "Aoba Johsai", "SSR"]
+    },
+    {
+      id: "iwaizumi(Practice)",
+      name: "Hajime Iwaizumi (Practice)",
+      img: "img-char/iwaizumiSR.png",
+      school: "Aoba Johsai",
+      rarity: "SSR",
+      position:"Wing Spiker",
+      stats: {
+        lvl1: {
+          Serve: 85,
+          Power_Attack: 99,
+          Set: 80,
+          Receive: 94,
+          Block: 89,
+          Save: 81
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Wing Spiker", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "matsukawa",
+      name: "Issei Matsukawa",
+      img: "img-char/matsukawaSSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 86,
+          Quick_Attack: 93,
+          Set: 81,
+          Receive: 84,
+          Block: 98,
+          Save: 79
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "hanamaki",
+      name: "Takahiro Hanamaki",
+      img: "img-char/hanamakiSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Wing Spiker",
+      stats: {
+        lvl1: {
+          Serve: 84,
+          Power_Attack: 89,
+          Set: 80,
+          Receive: 98,
+          Block: 89,
+          Save: 84
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Wing Spiker", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "yahaba",
+      name: "Shigeru Yahaba",
+      img: "img-char/yahabaSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Setter",
+      stats: {
+        lvl1: {
+          Serve: 98,
+          Power_Attack: 88,
+          Set: 93,
+          Receive: 80,
+          Block: 88,
+          Save: 75
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Setter", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "watari",
+      name: "Shinji Watari",
+      img: "img-char/watariSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Libero",
+      stats: {
+        lvl1: {
+          Serve: 81,
+          Power_Attack: 81,
+          Set: 89,
+          Receive: 98,
+          Block: 79,
+          Save: 94
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Libero", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "kindaichi",
+      name: "Yutaro Kindaichi",
+      img: "img-char/kindaichiSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 84,
+          Quick_Attack: 99,
+          Set: 79,
+          Receive: 84,
+          Block: 89,
+          Save: 88
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "kunimi",
+      name: "Akira Kunimi",
+      img: "img-char/kunimiSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Opposite Hitter",
+      stats: {
+        lvl1: {
+          Serve: 84,
+          Power_Attack: 98,
+          Set: 80,
+          Receive: 94,
+          Block: 89,
+          Save: 80
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Opposite Hitter", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "kyotani",
+      name: "Kentaro Kyotani",
+      img: "img-char/kyotaniSSR.png",
+      school: "Aoba Johsai",
+      rarity: "SSR",
+      position:"Opposite Hitter",
+      stats: {
+        lvl1: {
+          Serve: 116,
+          Power_Attack: 121,
+          Set: 97,
+          Receive: 108,
+          Block: 103,
+          Save: 97
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Opposite Hitter", "Aoba Johsai", "SSR"]
+    },
+    {
+      id: "kyotani(Practice)",
+      name: "Kentaro Kyotani (Practice)",
+      img: "img-char/kyotaniSR.png",
+      school: "Aoba Johsai",
+      rarity: "SR",
+      position:"Opposite Hitter",
+      stats: {
+        lvl1: {
+          Serve: 95,
+          Power_Attack: 99,
+          Set: 79,
+          Receive: 88,
+          Block: 84,
+          Save: 79
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Opposite Hitter", "Aoba Johsai", "SR"]
+    },
+    {
+      id: "kenma",
+      name: "Kenma Kozume",
+      img: "img-char/kenmaSSR.png",
+      school: "Nekoma",
+      rarity: "SSR",
+      position:"Setter",
+      stats: {
+        lvl1: {
+          Serve: 104,
+          Power_Attack: 114,
+          Set: 122,
+          Receive: 98,
+          Block: 114,
+          Save: 93
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Setter", "Nekoma", "SSR"]
+    },
+    {
+      id: "kenma(Practice)",
+      name: "Kenma Kozume (Practice)",
+      img: "img-char/kenmaSR.png",
+      school: "Nekoma",
+      rarity: "SR",
+      position:"Setter",
+      stats: {
+        lvl1: {
+          Serve: 85,
+          Power_Attack: 93,
+          Set: 99,
+          Receive: 80,
+          Block: 93,
+          Save: 73
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Setter", "Nekoma", "SR"]
+    },
+    {
+      id: "kuroo",
+      name: "Tetsuro Kuroo",
+      img: "img-char/kurooUR.png",
+      school: "Nekoma",
+      rarity: "UR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 123,
+          Quick_Attack: 133,
+          Set: 116,
+          Receive: 119,
+          Block: 140,
+          Save: 113
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Nekoma", "UR"]
+    },
+    {
+      id: "kuroo(Practice)",
+      name: "Tetsuro Kuroo (Practice)",
+      img: "img-char/kurooSSR.png",
+      school: "Nekoma",
+      rarity: "SSR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 107,
+          Quick_Attack: 115,
+          Set: 100,
+          Receive: 104,
+          Block: 122,
+          Save: 98
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Nekoma", "SSR"]
+    },
+    {
+      id: "yaku(Practice)",
+      name: "Morisuke Yaku (Practice)",
+      img: "img-char/yakuSR.png",
+      school: "Nekoma",
+      rarity: "SR",
+      position:"Libero",
+      stats: {
+        lvl1: {
+          Serve: 81,
+          Power_Attack: 81,
+          Set: 90,
+          Receive: 100,
+          Block: 79,
+          Save: 95
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Libero", "Nekoma", "SR"]
+    },
+    {
+      id: "lev",
+      name: "Lev Haiba",
+      img: "img-char/levSSR.png",
+      school: "Nekoma",
+      rarity: "SSR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 106,
+          Quick_Attack: 116,
+          Set: 100,
+          Receive: 102,
+          Block: 121,
+          Save: 96
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Nekoma", "SSR"]
+    },
+    {
+      id: "kenma",
+      name: "Lev Haiba (Practice)",
+      img: "img-char/levSR.png",
+      school: "Nekoma",
+      rarity: "SR",
+      position:"Middle Blocker",
+      stats: {
+        lvl1: {
+          Serve: 86,
+          Quick_Attack: 94,
+          Set: 81,
+          Receive: 84,
+          Block: 99,
+          Save: 79
+        },
+        max: {
+          Attack: 70,
+          Block: 65,
+          Defense: 67,
+          Receive: 70,
+          Speed: 75,
+          Stamina: 78,
+          Technique: 88
+        }
+      },
+      tags: ["Middle Blocker", "Nekoma", "SR"]
     },
   ];
 
